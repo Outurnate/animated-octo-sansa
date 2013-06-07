8f109a49365802f0c8e7eef300135581e60b2f29
josephd <joe@w00t42.net>
2013-06-07 09:37:53 -0400

    Updated render loop

--- glfw-thinger.cpp
+++ glfw-thinger.cpp
@@ -3,16 +3,42 @@
 #include <iostream>
 #include <GL/glfw.h>
 
+static void error(int error, const char* description)
+{
+}
+
+static void key(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
+}
+
 int main(void)
 {
+  GLFWwindow* window;
+
+  glfwSetErrorCallback(error);
   if (!glfwInit())
     return EXIT_FAILURE;
-  if (!glfwOpenWindow(640, 480, 8, 8, 8, 0, 24, 0, GLFW_WINDOW))
-    return EXIT_FAILURE;
-  while (glfwGetWindowParam(GLFW_OPENED))
+  if (!(window = glfwCreateWindow(640, 480, "asdf", NULL, NULL)))
   {
-    glfwSwapBuffers();
+    glfwTerminate();
+    return EXIT_FAILURE;
   }
+  glfwMakeContextCurrent(window);
+  glfwSetKeyCallback(window, key);
+  double last_time = 0.0;
+  while(!glfwWindowShouldClose(window))
+  {
+    int width, height;
+    double time = glfwGetTime(), delta = time - last_time;
+    glfwGetFramebufferSize(window, &width, &height);
+    //render
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+    last_time = time;
+  }
+  glfwDestroyWindow(window);
   glfwTerminate();
   return EXIT_SUCCESS;
 }
