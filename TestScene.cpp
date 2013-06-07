3684ccf0aac3c00cea0911cd06d695d854132137
josephd <joe@w00t42.net>
2013-06-07 12:48:39 -0400

    Create TestScene.cpp

---
+++ TestScene.cpp
@@ -0,0 +1,17 @@
+#include "TestScene.h"
+
+Scene::Scene()
+{
+}
+
+Scene::~Scene()
+{
+}
+
+void Scene::key(int key, int scancode, int action, int mods)
+{
+}
+
+void Scene::render(double delta, int width, int height)
+{
+}
