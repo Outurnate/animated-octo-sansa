a5cc32f2eb8daa87c92f16a808043760d55243f0
josephd <joe@w00t42.net>
2013-06-11 20:36:02 -0400

    Added fixed-function lighting

--- Scenes/TestScene.cpp
+++ Scenes/TestScene.cpp
@@ -4,28 +4,25 @@
 #include <string.h>
 #include <iostream>
 #include <tgmath.h>
-#include <glm/glm.hpp>
 #include <glm/gtc/noise.hpp>
 
 TestScene::TestScene(GLFWwindow* window)
-  : Scene(), map_width(256), map_height(256), map(new float[map_width * map_height]), map_normal(new float[map_width * map_height * 3]),
+  : Scene(), map_width(128), map_height(128), map(new float[map_width * map_height]), map_normal(new glm::vec3[map_width * map_height * 3]),
     current_pos({ 1.5f, 100.0f, 6.0f }),
-    key_w(false), key_a(false), key_s(false), key_d(false), key_space(false), key_shift(false), wireframe(false),
-    n_verticies_map(map_width * map_height * 3), n_indicies_map(map_width * map_height * 6), font_AverageMono("AverageMono.ttf")
+    key_w(false), key_a(false), key_s(false), key_d(false), key_space(false), key_shift(false), wireframe(false), lighting(true),
+    n_verticies_map(map_width * map_height * 12), n_indicies_map(map_width * map_height * 6), font_AverageMono("AverageMono.ttf")
 {
   for(unsigned x = 0; x < map_width; ++x)
     for(unsigned y = 0; y < map_height; ++y)
-      map[(y * map_width) + x] = 0;//(glm::simplex(glm::vec4(x / 8.0f, y / 8.0f, 0.5f, 0.5f)) * 10.0f) + (glm::simplex(glm::vec4(x / 32.0f, y / 32.0f, 0.5f, 0.5f)) * 100.0f);
+      map[(y * map_width) + x] = (glm::simplex(glm::vec4(x / 8.0f, y / 8.0f, 0.5f, 0.5f)) * 10.0f) + (glm::simplex(glm::vec4(x / 32.0f, y / 32.0f, 0.5f, 0.5f)) * 100.0f);
+  unsigned i = 0;
   for(unsigned x = 0; x < map_width; ++x)
     for(unsigned y = 0; y < map_height; ++y)
-    {
-      glm::vec3 normal = glm::normalize(glm::cross(
+      map_normal[i++] = glm::normalize(glm::cross(
       ((x == 0)                ? glm::vec3(x, map[(y * map_width) + x], y) : glm::vec3(x - 1, map[(y       * map_width) + x - 1], y)) -
       ((x == (map_width  - 1)) ? glm::vec3(x, map[(y * map_width) + x], y) : glm::vec3(x + 1, map[(y       * map_width) + x + 1], y)),
       ((y == 0)                ? glm::vec3(x, map[(y * map_width) + x], y) : glm::vec3(x,     map[((y - 1) * map_width) + x],    y - 1)) -
       ((y == (map_height - 1)) ? glm::vec3(x, map[(y * map_width) + x], y) : glm::vec3(x,     map[((y + 1) * map_width) + x],    y + 1))));
-      std::cout << normal.x << "," << normal.y << "," << normal.z << std::endl;
-    }
 }
 
 TestScene::~TestScene()
@@ -58,6 +55,9 @@ void TestScene::key(GLFWwindow* window, int key, int scancode, int action, int m
       break;
     case '1':
       wireframe = !wireframe;
+      break;
+    case '2':
+      lighting = !lighting;
       break;
     }
   else if (action == GLFW_RELEASE)
@@ -107,12 +107,20 @@ void TestScene::init(GLFWwindow* window)
   glEnable(GL_DEPTH_TEST);
   glDepthFunc(GL_LEQUAL);
 
-  glShadeModel(GL_SMOOTH);
+  glShadeModel(GL_FLAT);
+  glEnable(GL_NORMALIZE);
+
+  GLfloat ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f }; glLightfv(GL_LIGHT0, GL_AMBIENT,  ambient);
+  GLfloat diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f }; glLightfv(GL_LIGHT0, GL_DIFFUSE,  diffuse);
+  GLfloat specular[] = { 1.0f, 1.0f, 1.0f, 1.0f }; glLightfv(GL_LIGHT0, GL_SPECULAR, specular);
+  GLfloat position[] = { 100.0f, 100.0f, 0.0f, 1.0f }; glLightfv(GL_LIGHT0, GL_POSITION, position);
 
   glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_HIDDEN);
 
   GLfloat verticies_map[n_verticies_map];
   GLushort indicies_map[n_indicies_map];
+
+  // TODO: mixmap
 
   unsigned i = 0;
   for(unsigned x = 0; x < map_width; ++x)
@@ -121,6 +129,15 @@ void TestScene::init(GLFWwindow* window)
       verticies_map[i++] = x * 10;
       verticies_map[i++] = map[(y * map_width) + x];
       verticies_map[i++] = y * 10;
+      verticies_map[i++] = map_normal[(y * map_width) + x].x;
+      verticies_map[i++] = map_normal[(y * map_width) + x].y;
+      verticies_map[i++] = map_normal[(y * map_width) + x].z;
+      verticies_map[i++] = 0; // R
+      verticies_map[i++] = 0; // G
+      verticies_map[i++] = 0; // B
+      verticies_map[i++] = 0; // A
+      verticies_map[i++] = 0; // S
+      verticies_map[i++] = 0; // T
     }
 
   i = 0;
@@ -129,16 +146,17 @@ void TestScene::init(GLFWwindow* window)
     {
       indicies_map[i++] = (y * map_height) + x;
       indicies_map[i++] = (y * map_height) + x + 1;
-      indicies_map[i++] = (y * map_height) + x + 257;
-      indicies_map[i++] = (y * map_height) + x + 257;
-      indicies_map[i++] = (y * map_height) + x + 256;
+      indicies_map[i++] = (y * map_height) + x + map_height + 1;
+      indicies_map[i++] = (y * map_height) + x + map_height + 1;
+      indicies_map[i++] = (y * map_height) + x + map_height;
       indicies_map[i++] = (y * map_height) + x;
     }
 
   glGenBuffers(1, &map_vbo);
   glBindBuffer(GL_ARRAY_BUFFER, map_vbo);
   glBufferData(GL_ARRAY_BUFFER, sizeof(verticies_map), verticies_map, GL_STATIC_DRAW);
-  glVertexPointer(3, GL_FLOAT, 0, (GLvoid*)0);
+  glVertexPointer(3, GL_FLOAT, 12 * sizeof(GLfloat), (GLvoid*)0);
+  glNormalPointer(GL_FLOAT, 12 * sizeof(GLfloat), (GLvoid*)3);
 
   glGenBuffers(1, &map_ibo);
   glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, map_ibo);
@@ -177,37 +195,16 @@ void TestScene::render(GLFWwindow* window, double delta, int width, int height)
   glMatrixMode(GL_PROJECTION);
   glPushMatrix();
   glLoadIdentity();
-  glFrustum(-ar, ar, -1.0, 1.0, 2.0, 2560.0);
+  glFrustum(-ar, ar, -1.0, 1.0, 2.0, 5120.0);
   glMatrixMode(GL_MODELVIEW);
   glPushMatrix();
   glLoadIdentity();
 
   glClearColor(0.0, 0.0, 0.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-  glColor3d(0.0, 1.0, 0.0);
+  glColor3d(1.0, 1.0, 0.0);
 
   glPolygonMode(GL_FRONT_AND_BACK, wireframe ? GL_LINE : GL_FILL);
-
-  glEnable(GL_LIGHT0);
-  glEnable(GL_LIGHT1);
-  glEnable(GL_NORMALIZE);
-  glEnable(GL_COLOR_MATERIAL);
-  glEnable(GL_LIGHTING);
-
-  const GLfloat light_0_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-  const GLfloat light_0_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-  const GLfloat light_0_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-  const GLfloat light_0_position[] = { 0.0f, 5.0f, 5.0f, 0.0f };
-
-  const GLfloat light_1_ambient[]  = { 0.0f, 0.0f, 0.0f, 1.0f };
-  const GLfloat light_1_diffuse[]  = { 1.0f, 1.0f, 1.0f, 1.0f };
-  const GLfloat light_1_specular[] = { 1.0f, 1.0f, 1.0f, 1.0f };
-  const GLfloat light_1_position[] = { 0.0f, 0.0f, 0.0f, 1.0f };
-
-  const GLfloat mat_ambient[]    = { 0.7f, 0.7f, 0.7f, 1.0f };
-  const GLfloat mat_diffuse[]    = { 0.8f, 0.8f, 0.8f, 1.0f };
-  const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
-  const GLfloat high_shininess[] = { 100.0f };
 
   // TODO: use glm
   point3f dir({ (float)(cos(dy) * sin(dx)), (float)sin(dy), (float)(cos(dy) * cos(dx)) });
@@ -218,11 +215,13 @@ void TestScene::render(GLFWwindow* window, double delta, int width, int height)
 	    current_pos.x + dir.x, current_pos.y + dir.y, current_pos.z + dir.z,
 	    up.x,                  up.y,                  up.z);
 
-  // Default material
-  glMaterialfv(GL_FRONT, GL_AMBIENT,   mat_ambient);
-  glMaterialfv(GL_FRONT, GL_DIFFUSE,   mat_diffuse);
-  glMaterialfv(GL_FRONT, GL_SPECULAR,  mat_specular);
-  glMaterialfv(GL_FRONT, GL_SHININESS, high_shininess);
+  if (lighting)
+  {
+    glEnable(GL_LIGHTING);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHT0);
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+  }
 
   glEnableClientState(GL_VERTEX_ARRAY);
   glBindBuffer(GL_ARRAY_BUFFER, map_vbo);
@@ -230,19 +229,8 @@ void TestScene::render(GLFWwindow* window, double delta, int width, int height)
   glDrawElements(GL_TRIANGLES, n_indicies_map, GL_UNSIGNED_SHORT, NULL);
   glDisableClientState(GL_VERTEX_ARRAY);
 
-  // Directional light.  Sun
-  glLightfv(GL_LIGHT0, GL_AMBIENT,  light_0_ambient);
-  glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_0_diffuse);
-  glLightfv(GL_LIGHT0, GL_SPECULAR, light_0_specular);
-  glLightfv(GL_LIGHT0, GL_POSITION, light_0_position);
-
-  glLoadIdentity();
-
-  // Camera light
-  glLightfv(GL_LIGHT1, GL_AMBIENT,  light_1_ambient);
-  glLightfv(GL_LIGHT1, GL_DIFFUSE,  light_1_diffuse);
-  glLightfv(GL_LIGHT1, GL_SPECULAR, light_1_specular);
-  glLightfv(GL_LIGHT1, GL_POSITION, light_1_position);
+  glDisable(GL_COLOR_MATERIAL);
+  glDisable(GL_LIGHTING);
 
   glPixelTransferf(GL_RED_BIAS,  -1.0f);
   glPixelTransferf(GL_GREEN_BIAS, 0.0f);
--- Scenes/TestScene.h
+++ Scenes/TestScene.h
@@ -4,6 +4,7 @@
 #define GLFW_INCLUDE_GLU
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
 #include <FTGL/ftgl.h>
 
 #include "Scene.h"
@@ -15,9 +16,9 @@ class TestScene : public Scene
 private:
   unsigned map_width, map_height;
   float* map;
-  float* map_normal;
+  glm::vec3* map_normal;
   point3f current_pos;
-  bool key_w, key_a, key_s, key_d, key_space, key_shift, wireframe;
+  bool key_w, key_a, key_s, key_d, key_space, key_shift, wireframe, lighting;
   GLuint map_vbo, map_ibo;
   size_t n_verticies_map;
   size_t n_indicies_map;
