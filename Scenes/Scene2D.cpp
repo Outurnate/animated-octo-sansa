5230c17c13df25492c7d2ccfb509706c23972376
josephd <joe@w00t42.net>
2013-06-12 17:50:13 -0400

    Added missing scene files

---
+++ Scenes/Scene2D.cpp
@@ -0,0 +1,30 @@
+#include "Scene2D.h"
+
+Scene2D::Scene2D()
+  : Scene()
+{
+}
+
+Scene2D::~Scene2D()
+{
+}
+
+void Scene2D::init(GLFWwindow* window)
+{
+}
+
+void Scene2D::key(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+}
+
+void Scene2D::mouse(GLFWwindow* window, double x, double y)
+{
+}
+
+void Scene2D::render(GLFWwindow* window, double delta, int width, int height)
+{
+}
+
+void Scene2D::destroy(GLFWwindow* window)
+{
+}
