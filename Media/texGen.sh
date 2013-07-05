b5182e3f8edaa92e3d9a070f035eb7669b5f5ca9
josephd <joe@w00t42.net>
2013-07-05 17:54:10 -0400

    Fixed restructure

--- Media/texGen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd Textures
-java -cp ../Utilities:../NeoTextureEdit_0.6.4/lib/NeoTextureEdit.jar NeoGen 256 `ls *.256.tgr`
-java -cp ../Utilities:../NeoTextureEdit_0.6.4/lib/NeoTextureEdit.jar NeoGen 1024 `ls *.1024.tgr`
-mogrify -format tga *.png
-rm -f *.png
